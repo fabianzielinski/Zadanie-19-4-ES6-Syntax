2f3569ade7664ffdba5b542e3a1dce73649bd487
fabianzielinski <fabz@o2.pl>
2019-09-08 16:49:03 +0200

    Add Zad 5

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ const average = (...args) =>
   // zadanie 4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 average(...grades);
+
+// zadanie 5
+const array = [1, 4, "Iwona", false, "Nowak"];
+const [, , firstname, , lastname] = array;
+console.log(`${firstname} ${lastname}`);
