01d6fbf992855d2540bd36648c585e952ca443a4
fabianzielinski <fabz@o2.pl>
2019-09-08 15:57:09 +0200

    Add Zad 3

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ console.log(`${a} ${b}`);
 // zadanie 2
 const multiply = (a = 1, b = 1) => console.log(a * b);
 
+// zadanie 3
+const average = (...args) =>
+  console.log(args.reduce((total, arg) => total + arg) / args.length);
